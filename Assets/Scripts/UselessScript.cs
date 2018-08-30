a4094e9e7df1082a505db6503d2e5436e220aa1a
HOWARDDESKTOP\Howard Lehr <howard.lehr@gmail.com>
2018-08-29 18:47:52 -0700

    Added a Script

---
+++ Assets/Scripts/UselessScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UselessScript : MonoBehaviour {
+    
+    //this script has no purpose.  It's just to test GitHub.
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
